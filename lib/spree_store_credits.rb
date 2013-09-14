448538beb7ddf27bd0f6cf39760d9f6bac171d72
shenwei235 <shenwei235@gmail.com>
2013-09-14 14:33:22 +0800

    Update spree_store_credits.rb
    load c

--- lib/spree_store_credits.rb
+++ lib/spree_store_credits.rb
@@ -12,7 +12,8 @@ module SpreeStoreCredits
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env == "production" ? require(c) : load(c)
+        #Rails.env == "production" ? require(c) : load(c)
+        load(c)
       end
     end
 
